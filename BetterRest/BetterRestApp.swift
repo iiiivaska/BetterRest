a3b4d8c1fefd155cdceb826375c1ca88c18460d8
iiiivaska <iiiivaska@yandex.ru>
2021-09-27 13:29:52 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Василий Буланов on 9/27/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
