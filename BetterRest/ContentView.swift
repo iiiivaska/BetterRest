4d240dd6ce57106ba5553ed926ae73dfaba9663e
iiiivaska <iiiivaska@yandex.ru>
2021-09-29 17:45:49 +0300

    Replaced Vstack`s with Section, Replaced Stepper for coffee amount with Picker, Deleted Button "Calculate bedtime" and Alert, instead added Section with current bedtime

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -11,8 +11,6 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = defaultWakeTime
     @State private var coffeeAmount = 1
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
     @State private var showingAlert = false
     
     static var defaultWakeTime: Date {
@@ -23,48 +21,38 @@ struct ContentView: View {
     }
     var body: some View {
         NavigationView {
-        Form {
-            VStack(alignment: .leading, spacing: 0)
-            {
-                Text("When do you want to wake up?")
-                    .font(.headline)
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-            }
-            VStack(alignment: .leading, spacing: 0)
-            {
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-                    Text("\(sleepAmount, specifier: "%g") hours")
+            Form {
+                Section {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
                 }
-            }
-            VStack(alignment: .leading, spacing: 0)
-            {
-                Text("Daily coffee intake")
-                    .font(.headline)
-                Stepper(value: $coffeeAmount, in: 1...20) {
-                    if (coffeeAmount == 1) {
-                        Text("1 cup")
-                    } else {
-                        Text("\(coffeeAmount) cups")
+                Section {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
                     }
                 }
-            }
+                Section {
+                    Picker("Daily coffee intake", selection: $coffeeAmount) {
+                        ForEach(0..<12) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+                Section {
+                    Text(calculateBedtime())
+                }
             }
             .navigationTitle("BetterRest")
-            .navigationBarItems(trailing:
-                Button(action: calculateBedtime) {
-                    Text("Calculate")
-                }
-            )
-            .alert(isPresented: $showingAlert) {
-                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-            }
         }
     }
     
-    func calculateBedtime() {
+    func calculateBedtime() -> String {
+        var alertMessage = ""
+        var alertTitle = ""
         let model = SleepCalculator()
         let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
         let hour = (components.hour ?? 0) * 60 * 60
@@ -76,12 +64,12 @@ struct ContentView: View {
             formatter.timeStyle = .short
             
             alertMessage = formatter.string(from: sleepTime)
-            alertTitle = "Your ideal bedtime is "
+            alertTitle = "Your ideal bedtime is"
         } catch {
             alertTitle = "Error"
             alertMessage = "Something went wrong, sorry."
         }
-        showingAlert = true
+        return ("\(alertTitle) \(alertMessage)")
     }
 }
 
